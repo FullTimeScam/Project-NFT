c115711537e7cae768ef5c2933cbe2769dce2983
FullTimeScam <418.gingin@gmail.com>
2024-06-14 15:00:20 +0900

    vite 추가

---
+++ contracts/Mint.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+contract MintNft is ERC721Enumerable, Ownable {
+    string metadataUri;
+    string unrevealedUri;
+
+    
+    mapping(uint => bool) isRevealed;
+
+
+    constructor(string memory _name, string memory _symbol, string memory _metadataUri, string memory _unrevealedUri) ERC721(_name, _symbol) Ownable(msg.sender) {
+        metadataUri = _metadataUri;
+        unrevealedUri = _unrevealedUri;
+    }
+
+    function mintNft() public {
+        require(totalSupply() < 9, "No more mint.");
+
+        uint tokenId = totalSupply() + 1;
+
+        _mint(msg.sender, tokenId);
+    }
+
+    function tokenURI(uint _tokenId) public view override returns (string memory) {
+        if(isRevealed[_tokenId]) {
+            return string(abi.encodePacked(metadataUri, Strings.toString(_tokenId), ".json")); 
+        } else {
+            return unrevealedUri;
+        }
+    }
+
+    function setIsReveal(uint _tokenId) public  {
+        require(msg.sender == ownerOf(_tokenId), "NONONO!!" );
+
+        isRevealed[_tokenId] = true;
+    }
+}
---
+++ vite/README.md
@@ -0,0 +1,1 @@
+Mint.sol에 reveal기능 변경 - NFT보유자만 자기 NFT를 리빌 할 수 있음
